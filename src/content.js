d68ae0e9c6efc1f13e25ce16188f1c59fbcd6fc6
iitenkida7 <t.ishii@teradox.net>
2021-07-02 09:59:44 +0900

    Add  indert element

--- src/content.js
+++ src/content.js
@@ -1,14 +1,17 @@
 chrome.storage.sync.get("version", function (value) {
     if (typeof value.version === 'undefined') {
         return false;
-
-    } else {
-        if (getLaravelVersion() != value.version) {
-            $originUrl = parseUrl();
-            $originUrl[2] = value.version;
-            location.href = $originUrl.join('/');
-        }
+    } else if (getLaravelVersion() != value.version) {
+        saveTmpRedirect(getLaravelVersion(), value.version);
+        location.href = getNewUri(value.version);
+    } else if (getLaravelVersion() == value.version) {
+        chrome.storage.local.get("hasRedirect", function (value) {
+            console.log(value); // debug
+            chrome.storage.local.remove("hasRedirect", function () { });
+            document.body.insertAdjacentHTML("beforeend", "<div class='chrome-plugin'>@todo: モーダル</div>");
+        });
     }
+
 });
 
 function parseUrl() {
@@ -24,4 +27,19 @@ function parseUrl() {
 
 function getLaravelVersion() {
     return location.pathname.split('/')[2];
+}
+
+function getNewUri(newVersion) {
+    originUrl = parseUrl();
+    originUrl[2] = newVersion
+    return originUrl.join('/');
+}
+
+function saveTmpRedirect(oldVersion, newVersion) {
+    chrome.storage.local.set({
+        'hasRedirect': {
+            'old': oldVersion,
+            'new': newVersion,
+        }
+    });
 }
